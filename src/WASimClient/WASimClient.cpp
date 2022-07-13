mpaperno/WASimCommander

commit 61c4cfe1f2a665de72f375039aca469433502972

[WASimClient] Reset "list pending" flag in connectServer() (may fix offhand chance of the flag being "stuck"); - Add check for server still being connected while awaiting command responses; - Make list result timeout value atomic to avoid (or at least offload) the mutex lock.

src/WASimClient/WASimClient.cpp
@@ -187,7 +187,7 @@ class WASimClient::Private
 		atomic<LookupItemType> listType { LookupItemType::None };
 		uint32_t token { 0 };
 		shared_ptr<condition_variable_any> cv = make_shared<condition_variable_any>();
-		Clock::time_point nextTimeout {};
+		atomic<Clock::time_point> nextTimeout {};
 		ListResult::listResult_t result;
 		shared_mutex mutex;
 
@@ -687,9 +687,10 @@ class WASimClient::Private
 			LOG_WRN << "Server version does not match WASimClient version " << STREAM_HEX8(WSMCMND_VERSION);
 		setStatus(ClientStatus::Connected);
 
+		// clear any pending list request (unlikely)
+		listResult.reset();
 		// make sure server knows our desired log level and set up data area/request if needed
 		updateServerLogLevel();
-
 		// (re-)register (or delete) any saved DataRequests
 		registerAllDataRequests();
 		// same with calculator events
@@ -810,7 +811,7 @@ class WASimClient::Private
 			timeout = settings.networkTimeout;
 
 		auto stop_waiting = [=]() {
-			return tr->response.commandId != CommandId::None && tr->response.token == token && (!extraPredicate || extraPredicate());
+			return !serverConnected || (tr->response.commandId != CommandId::None && tr->response.token == token && (!extraPredicate || extraPredicate()));
 		};
 
 		HRESULT hr = E_TIMEOUT;
@@ -850,8 +851,8 @@ class WASimClient::Private
 	void waitListRequestEnd()
 	{
 		auto stop_waiting = [this]() {
-			shared_lock lock(listResult.mutex);
-			return listResult.nextTimeout >= Clock::now();
+			//shared_lock lock(listResult.mutex);
+			return listResult.nextTimeout.load() >= Clock::now();
 		};
 
 		Command response;

commit 7d105070db02238a00f311df0b9f8c2c3ade0975

[WASimClient] Fixed bug introduced in 61c4cfe1f that prevented Client from completing connection to Server on first attempt.

src/WASimClient/WASimClient.cpp
@@ -665,23 +665,13 @@ class WASimClient::Private
 		}
 		// The connection response, if any, is written by the server into the "command response" client data area, CLI_DATA_ID_RESPONSE, processed in SIMCONNECT_RECV_ID_CLIENT_DATA
 
-		shared_ptr<condition_variable_any> cv = make_shared<condition_variable_any>();
-		Command response;
-		TrackedResponse *tr = enqueueTrackedResponse(clientId, weak_ptr(cv));
-		hr = waitCommandResponse(tr->token, &response, timeout);
-
-		if (hr == E_TIMEOUT) {
-			LOG_ERR << "Connection request timed out after " << timeout << "ms.";
+		// wait until server connects or times out or disconnectSimulator() is called.
+		if (!waitCondition([&]() { return !runDispatchLoop || serverConnected; }, timeout)) {
+			LOG_ERR << "Server connection timed out or refused after " << timeout << "ms.";
 			setStatus(ClientStatus::SimConnected);
 			return E_TIMEOUT;
 		}
-		if (response.commandId != CommandId::Ack || !serverConnected) {
-			LOG_WRN << "Server refused connection! Reason, if any: " << quoted(response.sData);
-			setStatus(ClientStatus::SimConnected);
-			return E_FAIL;
-		}
 
-		serverVersion = (uint32_t)response.fData;
 		LOG_INF << "Connected to " WSMCMND_PROJECT_NAME " server v" << STREAM_HEX8(serverVersion);
 		if (serverVersion != WSMCMND_VERSION)
 			LOG_WRN << "Server version does not match WASimClient version " << STREAM_HEX8(WSMCMND_VERSION);
@@ -1280,6 +1270,8 @@ class WASimClient::Private
 									// Connected response
 									case CommandId::Connect:
 										serverConnected = cmd->commandId == CommandId::Ack && cmd->token == clientId;
+										serverVersion = (uint32_t)cmd->fData;
+										checkTracking = false;
 										break;
 									default:
 										break;